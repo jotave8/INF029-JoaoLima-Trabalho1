303d28cb083de958d4fd369b85aeda23805c566f
jotave8 <128985376+jotave8@users.noreply.github.com>
2024-11-20 14:01:19 +0000

    alteracao

--- novoCorretor.c
+++ novoCorretor.c
@@ -2,7 +2,7 @@
 #include<string.h>
 
 //renomeie o arquivo do include abaixo para PrimeiroUltimoNomeMATRICULA.h
-#include "PrimeiroUltimoNomeMATRICULA.h"
+#include "JoaoLima20241160026.h"
 
 void testQ1();
 void testQ2();
